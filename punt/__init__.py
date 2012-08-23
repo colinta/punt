4c5806b4b2e0317bc209e97c671d569940614562
Colin Thomas-Arnold <colinta@gmail.com>
2012-08-23 11:05:57 -0600

    version bump - to fix --version option

--- punt/__init__.py
+++ punt/__init__.py
@@ -23,7 +23,7 @@ from watchdog.events import FileSystemEventHandler
 
 
 def run():
-    arguments = docopt(__doc__, version='punt v1.7')
+    arguments = docopt(__doc__, version='punt v1.8.1')
 
     if not arguments['-w']:
         watch_paths = [os.getcwd()]
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 setup(
         name="punt",
-        version="v1.8",
+        version="v1.8.1",
         author="Colin Thomas-Arnold",
         author_email="colinta@gmail",
         url="https://github.com/colinta/punt",
